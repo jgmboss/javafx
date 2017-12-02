229412fb24ce9c91b56e85ae419f04c32bf90cb4
jgmboss <jgurung.62063@farnborough.ac.uk>
2017-12-02 00:03:45 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
-import Model.*;
+package src;
+
+//import Model.*;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -12,58 +14,61 @@ import javafx.scene.control.TabPane;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.beans.property.SimpleStringProperty;
-
+import javafx.scene.control.Label;
 
 import java.util.ArrayList;
 
-
 public class Main extends Application{
-
-
-
-
     //Under 'public class Main extends Application {'
     @Override
     public void start (Stage stage) throws Exception {
         Pane root = new Pane();
-        Scene scene = new Scene(root, 768, 468);
+        Scene scene = new Scene(root, 586, 350);
 
         stage.setTitle("Inventory");
         stage.setScene(scene);
         stage.show();
 
-        scene.getStylesheets().add("CSS.css");
+        //scene.getStylesheets().add("CSS.css");
 
-        HBox subSection = new HBox();
+        Pane subSection = new Pane();
         Button myButton = new Button("Enter");
-        myButton.setPrefSize(100, 35);
+        myButton.setPrefSize(162, 31);
         myButton.setOnAction((ActionEvent ae) -> System.out.println("hello"));
-        myButton.setTranslateX(330);
-        myButton.setTranslateY(400);
+        myButton.setTranslateX(212);
+        myButton.setTranslateY(192);
         root.getChildren().add(myButton);
-        centerPane.setAlignment(Pos.CENTER);
-        BorderPane.setAlignment(centerPane, Pos.CENTER);
-        centerPane.getStyleClass().add("centerPane");
+        subSection.setPrefSize(586, 263);
+        subSection.setTranslateX(0);
+        subSection.setTranslateY(80);
         
-        
-        
-
-    }
-    public void accounts (Stage stage) throws Exception {
-
-        ObservableList<Users> users = FXCollections.observableArrayList(
-                new Users("Jacob", ":)", "smith"),
-                new Users("Isabella", ":)", "Johnson"),
-                new Users("Ethan", ":)", "Williams"),
-                new Users("Emma", ":)", "Jones"),
-                new Users("Michael", ":)", "brown")
+        subSection.setStyle( "-fx-background-color: Darkgray;"
         );
+        
+        Label welcome = new Label("Welcome to the");
+        Label inventory = new Label("inventory application");
+        welcome.setTranslateX(230);
+        welcome.setTranslateY(38);
+        inventory.setTranslateX(209);
+        inventory.setTranslateY(57);
+        
+       // centerPane.setStyle("-fx-background-color: red");
 
     }
+
+    /* public void accounts (Stage stage) throws Exception {
+
+    ObservableList<Users> users = FXCollections.observableArrayList(
+    new Users("Jacob", ":)", "smith"),
+    new Users("Isabella", ":)", "Johnson"),
+    new Users("Ethan", ":)", "Williams"),
+    new Users("Emma", ":)", "Jones"),
+    new Users("Michael", ":)", "brown")
+    );
+     */
 
     public static void main(String[] args) {
-    launch(args);
-        }
+        launch(args);
+    }
 }
-
 
