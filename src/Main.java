00066cef35cbda99edaf192c180a55bca1adc3cd
jgmboss <jgurung.62063@farnborough.ac.uk>
2017-12-04 00:04:10 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -19,31 +19,42 @@ import javafx.scene.control.Label;
 import java.util.ArrayList;
 
 public class Main extends Application{
+    
+Scene start;
+Scene login;
+Scene signUp;
+Scene managerWelcome;
+Scene welcome;
+Scene managingAccounts;
+Scene settings;
+Scene view;
+Scene viewOne; 
+Scene edit;
+    
     //Under 'public class Main extends Application {'
     @Override
-    public void start (Stage stage) throws Exception {
-        Pane root = new Pane();
-        Scene scene = new Scene(root, 586, 350);
-
+    public void Start (Stage stage) throws Exception {
+        
         stage.setTitle("Inventory");
-        stage.setScene(scene);
+        stage.setScene(start);
         stage.show();
 
         //scene.getStylesheets().add("CSS.css");
 
-        Pane subSection = new Pane();
-        Button myButton = new Button("Enter");
-        myButton.setPrefSize(162, 31);
-        myButton.setOnAction((ActionEvent ae) -> System.out.println("hello"));
-        myButton.setTranslateX(212);
-        myButton.setTranslateY(192);
-        root.getChildren().add(myButton);
+        Pane subSection = new Pane(); //this will be the template for all the scenes so it looks professional 
         subSection.setPrefSize(586, 263);
         subSection.setTranslateX(0);
         subSection.setTranslateY(80);
-        
         subSection.setStyle( "-fx-background-color: Darkgray;"
         );
+        
+        Scene start = new Scene(subSection, 586, 350); //creates new scene with my template and the size of the scene (which will always be the same)
+        //these are things that will be in the start scene
+        Button toLogin = new Button("Enter");
+        toLogin.setPrefSize(162, 31);
+        toLogin.setOnAction((ActionEvent ae) -> stage.setScene(login)); //once clicked, login scene opens
+        toLogin.setTranslateX(212);
+        toLogin.setTranslateY(192);
         
         Label welcome = new Label("Welcome to the");
         Label inventory = new Label("inventory application");
@@ -52,9 +63,65 @@ public class Main extends Application{
         inventory.setTranslateX(209);
         inventory.setTranslateY(57);
         
+        
+        Scene login = new Scene(subSection, 586, 350);
+        
+        Label login = new Label("Login");
+        welcome.setTranslateX(230);
+        welcome.setTranslateY(38);
+        
+        Label login = new Label("Username");
+        welcome.setTranslateX(230);
+        welcome.setTranslateY(42);
+        //input code
+        
+        Label login = new Label("Password");
+        welcome.setTranslateX(230);
+        welcome.setTranslateY(59);
+        //input code
+        
+        Button toWelcome = new Button("Enter");
+        toWelcome.setPrefSize(162, 31);
+        toWelcome.setOnAction((ActionEvent ae) -> stage.setScene(login)); //once clicked, login scene opens
+        toWelcome.setTranslateX(212);
+        toWelcome.setTranslateY(192);
+        
+        Button toSignup = new Button("Sign Up");
+        toSignup.setPrefSize(162, 31);
+        toSignup.setOnAction((ActionEvent ae) -> stage.setScene(signUp)); //once clicked, login scene opens
+        toSignup.setTranslateX(560);
+        toSignup.setTranslateY(300);
+        
+        
+        Scene signUp = new Scene(subSection, 586, 350);
+        Label login = new Label("Login");
+        welcome.setTranslateX(230);
+        welcome.setTranslateY(38);
+        
+        Scene managerWelcome = new Scene(subSection, 586, 350);
+        
+        
+        Scene welcome = new Scene(subSection, 586, 350);
+        
+        
+        Scene managingAccounts = new Scene(subSection, 586, 350);
+        
+        
+        Scene settings = new Scene(subSection, 586, 350);
+        
+        
+        Scene view = new Scene(subSection, 586, 350);
+        
+        
+        Scene viewOne = new Scene(subSection, 586, 350);
+        
+        
+        Scene edit = new Scene(subSection, 586, 350);
+        
        // centerPane.setStyle("-fx-background-color: red");
-
+        start = new Scene(subSection
     }
+    
 
     /* public void accounts (Stage stage) throws Exception {
 
